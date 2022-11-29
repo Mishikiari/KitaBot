bf6be4c97a98e3fdfd96b359c655a9d0d650565a
Criss <xdcris05@gmail.com>
2022-11-29 18:01:00 +0100

    primo commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+import os
+import telebot
+import requests
+import bs4
+
+from grabber import getSakuga
+
+API_KEY = os.getenv('API_KEY')
+bot = telebot.TeleBot(API_KEY)
+sadD = os.getenv('sakD')
+
+
+@bot.message_handler(commands=['daily'])
+def daily(message):
+  fSak=getSakuga()
+  
+  bot.send_message(message.chat.id, fSak)
+
+
+@bot.message_handler(commands=['info'])
+def info(message):
+  bot.send_message(message.chat.id, "https://github.com/Mishikiari/SakBot")
+
+bot.polling()
