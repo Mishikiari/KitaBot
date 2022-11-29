7538148658416d845c5765ee7412234fe05b7961
Criss <xdcris05@gmail.com>
2022-11-29 18:08:48 +0100

    sium

--- Grabber.py
+++ Grabber.py
@@ -1,13 +1,15 @@
 import bs4, requests, webbrowser
 
-url = "https://www.sakugabooru.com/post/random"
-bUrl = "https://www.sakugabooru.com/data/"
+def getSakuga():
+  url = "https://www.sakugabooru.com/post/random"
+  bUrl = "https://www.sakugabooru.com/data/"
 
-response = requests.get(url)
-response.raise_for_status()
-soup = bs4.BeautifulSoup(response.text, 'html.parser')
-divSak = soup.find(class_='original-file-unchanged')
-sakD = divSak.find(bUrl)
-fSak = str(divSak.get('href'))
-if bUrl in fSak:
+  response = requests.get(url)
+  response.raise_for_status()
+  soup = bs4.BeautifulSoup(response.text, 'html.parser')
+  divSak = soup.find(class_='original-file-unchanged')
+  sakD = divSak.find(bUrl)
+  fSak = str(divSak.get('href'))
+  if bUrl in fSak:
     print(fSak)
+    return fSak
